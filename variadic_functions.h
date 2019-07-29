5514e6566c6a0b75c83f326125bfa28444e3922f
Miuywu <859@holbertonschool.com>
2019-07-29 21:11:07 +0000

    removed task 0,1,2 protos

--- variadic_functions.h
+++ variadic_functions.h
@@ -7,9 +7,6 @@
 
 
 void _putchar(char);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 void op_char(va_list list);
