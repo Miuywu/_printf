655aef538326b94a71309b28cdae32056d85c20c
Miuywu <859@holbertonschool.com>
2019-07-29 21:06:16 +0000

    removed diff print fns

--- 3-print_all.c
+++ 3-print_all.c
@@ -3,54 +3,6 @@
 #include <stdarg.h>
 
 /**
- * op_char - calls function
- * @list: string struct
- *
- * Return: 0
- */
-void op_char(va_list list)
-{
-	printf("%c", va_arg(list,  int));
-}
-/**
- * op_int - calls function
- * @list: string struct
- *
- * Return: 0
- */
-void op_int(va_list list)
-{
-	printf("%d", va_arg(list, int));
-}
-/**
- * op_float - calls function
- * @list: string struct
- *
- * Return: 0
- */
-void op_float(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-/**
- * op_char_ptr - calls function
- * @list: string struct
- *
- * Return: 0
- */
-void op_char_ptr(va_list list)
-{
-	char *str;
-
-	str = va_arg(list, char *);
-	if (str == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", str);
-}
-/**
  * print_all - calls function
  * @format: list of types of arguments passed to the function
  * Description: Function that prints anything
